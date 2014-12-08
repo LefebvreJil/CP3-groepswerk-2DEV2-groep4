98480a967e1a72cd5cc4ad1567dce2339b28ec56
LefebvreJil <jil.lefebvre@gmail.com>
2014-12-08 18:29:48 +0100

    Css files opgesplitst. 1 css file / pagina

--- js/script.js
+++ js/script.js
@@ -11,25 +11,25 @@
 		var imageInput = document.querySelector("input[name=image]");
             var errorElement = imageInput.parentNode.querySelector(".error-message");
             imageInput.addEventListener("change", function(e){
-                    errorElement.style.display="none";
-                    var img=imageInput.parentNode.querySelector("img");
-                    if(img){ imageInput.parentNode.removeChild(img);}
-                    if(imageInput.files.length>0){
-                            var file = imageInput.files['0'];
-                            var error = "";
-                            if(file.type.search("image") !==0){
-                                    errorElement.innerText = "the selected file is not an image";
-                                    errorElement.style.display = "block";
-                            }else{
-                                    var reader = new FileReader();
-                                    reader.onload = function(e){
-                                            var img = document.createElement("img");
-                                            img.onload=function(e){imageInput.parentNode.appendChild(img);};
-                                            img.setAttribute("src", reader.result);
-                                    };
-                                    reader.readAsDataURL(file);
-                            }
+                errorElement.style.display="none";
+                var img=imageInput.parentNode.querySelector("img");
+                if(img){ imageInput.parentNode.removeChild(img);}
+                if(imageInput.files.length>0){
+                    var file = imageInput.files['0'];
+                    var error = "";
+                    if(file.type.search("image") !==0){
+                            errorElement.innerText = "the selected file is not an image";
+                            errorElement.style.display = "block";
+                    }else{
+                        var reader = new FileReader();
+                        reader.onload = function(e){
+                            var img = document.createElement("img");
+                            img.onload=function(e){imageInput.parentNode.appendChild(img);};
+                            img.setAttribute("src", reader.result);
+                        };
+                        reader.readAsDataURL(file);
                     }
+                }
             });
 
 
