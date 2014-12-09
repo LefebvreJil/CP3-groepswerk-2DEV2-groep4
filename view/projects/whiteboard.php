c8a2181a663d0f7ca44fe09871daa66d8108501d
LefebvreJil <jil.lefebvre@gmail.com>
2014-12-09 19:19:56 +0100

    Html whiteboard aangepast

--- view/projects/whiteboard.php
+++ view/projects/whiteboard.php
@@ -1,21 +1,24 @@
 <div class="wrapper">
 	<section class="wNav">
-		<a href="index.php?page=projects">Terug</a>
-		<header>
-			<h1>Title van project</h1>
-		</header>
-		<a href="" class="addBtn">Add</a>
-		<a href="index.php?page=logout" class="logoutBtn">Logout</a>
+		<ul>
+			<li><a href="index.php?page=projects">Terug</a></li>
+			<li><header>
+				<h1>Title van project</h1>
+			</header></li>
+			<li><a href="" class="addBtn">Add</a></li>
+			<li class="lastLi"><a href="index.php?page=logout" class="logoutBtn">Logout</a></li>
+		</ul>
 	</section>
 	<div class="addObject">
 		<ul>
 			<li><a href="">Afbeelding</a></li>
 			<li><a href="">Video</a></li>
-			<li><a href="">Tekst</a></li>
 			<li><a href="">Post-it</a></li>
-			<li><a href="">Teken</a></li>
+			<li><a href="">Todo Lijst</a></li>
+			<li class="lastLi"><a href="">Tekenen</a></li>
 		</ul>
 	</div>
 	<div class="whiteboard">
 	</div>
+	<a href="" class="end">Keer terug naar het projectoverzicht</a>
 </div>
