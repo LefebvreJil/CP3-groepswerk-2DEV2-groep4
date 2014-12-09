cb7b823ccbe08d9b069d3d1a08b7139c7491623a
DelphiClaer <delphine.claerhout2@student.howest.be>
2014-12-09 18:22:46 +0100

    Classes toevoegen

---
+++ js/classes/stickyNote.js
@@ -0,0 +1,12 @@
+module.exports = (function(){
+
+	function Class() {
+		//console.log("[Class] Hello Jil");
+	}
+
+	/*Class.prototype.clickHandler = function(event){
+		//event.preventDefault();
+	}*/
+
+	return Class;
+})();
