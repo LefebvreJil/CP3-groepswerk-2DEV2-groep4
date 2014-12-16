ca8f7a2626536e4a6d3e9ad2c434bffdc4738f1a
DelphiClaer <delphine.claerhout2@student.howest.be>
2014-12-16 17:24:17 +0100

    syncen

--- js/classes/img.js
+++ js/classes/img.js
@@ -1,15 +1,7 @@
 module.exports = (function(){
 
-	//drag n drop
-    var dragNdrop = require('./dragNdrop.js');
-    
-
 	function Img() {
 		//console.log("[Img] Hello Jil");
-
-		var selectAllImg = $('.img-object');
-		console.log(selectAllImg);
-		new dragNdrop();
 	}
 
 	return Img;
